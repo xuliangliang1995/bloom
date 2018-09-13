dbffc1d03488198323a605c1cc9f1e13d3a68843
bloom <39378926+xuliangliang1995@users.noreply.github.com>
2018-09-13 12:04:21 +0800

    Update KeFuMsgNewsTest.java

--- src/main/java/com/bloom/domain/wechat/common/consumer/bean/KeFuMsgNewsTest.java
+++ src/main/java/com/bloom/domain/wechat/common/consumer/bean/KeFuMsgNewsTest.java
@@ -17,8 +17,9 @@ public class KeFuMsgNewsTest extends AbstractConsumerBean {
 			article.setPicUrl("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536778670264&di=07c7563309915c3a239345a3820d6f3b&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01cc13554242c80000019ae9e173e9.jpg%401280w_1l_2o_100sh.jpg");
 			article.setDescription("我慢慢的溶化在泥土里,翅膀上仿若有你的鼻息...");
 			article.setUrl("https://music.163.com/#/song?id=29792835&market=baiduqk");
+			WxArticle[] articles = {article};
 			WxMpXmlOutMessage wxMpXmlOutMessage = WxMpXmlOutMessage.NEWS()
-					.addArticle(article)
+					.addArticle(articles)
 					.fromUser(ctx.getWxMessage().getToUser())
 					.toUser(ctx.getWxMessage().getFromUser())
 					.build();
