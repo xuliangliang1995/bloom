edc88c8c46cc2e2cea99a343a0fe6ec5bb15889a
xuliangliang <835547206@qq.com>
2018-12-25 23:41:41 +0800

    添加SignServiceImplTest单元测试

--- src/test/java/com/bloom/domain/gardener/SignServiceImplTest.java
+++ src/test/java/com/bloom/domain/gardener/SignServiceImplTest.java
@@ -4,122 +4,96 @@
 package com.bloom.domain.gardener;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
 
-import javax.annotation.Resource;
-
-import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import com.bloom.BaseJunitTest;
 import com.bloom.dao.po.Gardener;
-import com.bloom.exception.IncorrectAccountException;
-import com.bloom.exception.IncorrectPasswordException;
-import com.bloom.exception.ServiceException;
+import com.bloom.dao.po.Role;
+import com.bloom.domain.gardener.impl.SignServiceImpl;
 import com.bloom.web.gardener.vo.SignUpForm;
 
 /**
- * <p>Title: SignServiceImplTest.java<／p>
- * <p>Description: <／p>
+ * <p>Title: SignServiceImplTest2.java<／p>
+ * <p>Description: SignServiceImpl测试类<／p>
  * <p>Copyright: Copyright (c) 2019<／p>
  * <p>Company: grasswort<／p>
  *
  * @author 树林里面丢了鞋
- * @date 2018年12月23日
+ * @date 2018年12月24日
  * @version 1.0
 */
-public class SignServiceImplTest extends BaseJunitTest{
-	@Resource
-	private SignService signServiceImpl;
-	
-	private String username;
-	private String password;
-	private String email;
-	private String nickName;
-	private SignUpForm form;
-	
-	@Before
-	public void setUp() {
-		while (true) {
-			username = RandomStringUtils.randomNumeric(6);
-			password = RandomStringUtils.randomNumeric(8);
-			try {
-				signServiceImpl.signIn(username, password);
-			} catch (ServiceException e) {
-				break;
-			}
-		}
-		email = "835547206@qq.com";
-		nickName = "shyCode";
-		form = new SignUpForm();
-		form.setUsername(username);
-		form.setPassword(password);
-		form.setNickName(nickName);
-		form.setEmail(email);
-	}
-	
+@RunWith(MockitoJUnitRunner.class)  
+public class SignServiceImplTest{
 	/**
-	 * 
-	 * <p>Title: testSignUpAndSignIn</p>
-	 * <p>Description: 测试注册和登录</p>
-	 * void
+	 * 测试对象
+	 */
+	@InjectMocks private SignServiceImpl signServiceImpl;
+	/**
+	 * 模拟Dao层对象
+	 */
+	@Spy private GardenerExtDaoMock gardenerExtDao;
+	/**
+	 * 模拟角色服务
+	 */
+	@Mock private RoleService roleService;
+	/**
+	 * Test method for {@link com.bloom.domain.gardener.impl.SignServiceImpl#signUp(com.bloom.web.gardener.vo.SignUpForm)}.
 	 */
 	@Test
-	@Rollback(value = true)
-	@Transactional
 	public void testSignUpAndSignIn() {
-		this.signUp();
-		Gardener gardener = signServiceImpl.signIn(username, password);
-		assertNotNull(gardener);
-	}
-	
-	/**
-	 * 
-	 * <p>Title: testSignUpAndSignInWithIncorrectUsername</p>
-	 * <p>Description: 错误账号登录</p>
-	 * void
-	 */
-	@Test(expected = IncorrectAccountException.class)
-	@Rollback(value = true)
-	@Transactional
-	public void testSignUpAndSignInWithIncorrectUsername() {
-		this.signUp();
-		signServiceImpl.signIn(username + password, password);
-	}
-	
-	/**
-	 * 
-	 * <p>Title: testSignUpAndSignInWithIncorrectPassword</p>
-	 * <p>Description: 错误密码登录</p>
-	 * void
-	 */
-	@Test(expected = IncorrectPasswordException.class)
-	@Rollback(value = true)
-	@Transactional
-	public void testSignUpAndSignInWithIncorrectPassword() {
-		this.signUp();
-		signServiceImpl.signIn(username , username + password);
-	}
-	
-	@After
-	public void after() {
-		username = null;
-		password = null;
-		email = null;
-		nickName = null;
-		form = null;
-	}
-	
-	/**
-	 * 
-	 * <p>Title: signUp</p>
-	 * <p>Description: 注册</p>
-	 * void
-	 */
-	private void signUp() {
+		/// 录制模拟对象行为(之前Mock gardenerExtDao时的代码，留作参考)
+		/*// 1、根据用户名（加密后）从数据库中查询对象，永远返回null
+		when(gardenerExtDao.selectKeyByUsername(anyString())).thenReturn(null);
+		// 2、向数据库中插入对象，id永远为1
+		when(gardenerExtDao.insert(any(Gardener.class))).thenAnswer(new Answer<Integer>() {
+			public Integer answer(InvocationOnMock invocation) {
+				Gardener gardener = invocation.getArgument(0);
+				gardener.setId(1);
+		         return 1;
+		     }
+		});*/
+		// 录制默认角色ID为1
+		final Role defaultRole = new Role();
+		defaultRole.setId(1);
+		when(roleService.defaultRole()).thenReturn(defaultRole);
+		// 准备数据
+		final String USERNAME = "grasswort";
+		final String PASSWORD = "xol4l2y2xx";
+		SignUpForm form = new SignUpForm();
+		form.setUsername(USERNAME);
+		form.setPassword(PASSWORD);
+		form.setNickName("树林里面丢了鞋");
+		form.setEmail("835547206@qq.com");
+		// 注册
 		signServiceImpl.signUp(form);
+		// 登录
+		Gardener gardener = signServiceImpl.signIn(USERNAME, PASSWORD);
+		assertNotNull(gardener);
+		assertNotNull(gardener.getId());
 	}
+
+	/**
+	 * Test method for {@link com.bloom.domain.gardener.impl.SignServiceImpl#signInByWechatOpenId(javax.servlet.http.HttpServletRequest, java.lang.String, java.lang.String)}.
+	 */
+	@Ignore
+	public void testSignInByWechatOpenId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.bloom.domain.gardener.impl.SignServiceImpl#signOut(javax.servlet.http.HttpServletRequest)}.
+	 */
+	@Ignore
+	public void testSignOut() {
+		fail("Not yet implemented");
+	}
+
 }
