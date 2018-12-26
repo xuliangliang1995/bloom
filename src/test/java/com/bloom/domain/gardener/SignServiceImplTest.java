6a7a9ba35b654601e6273aecedd3200127fab8ec
xuliangliang <835547206@qq.com>
2018-12-26 23:54:11 +0800

    删掉无用引用

--- src/test/java/com/bloom/domain/gardener/SignServiceImplTest.java
+++ src/test/java/com/bloom/domain/gardener/SignServiceImplTest.java
@@ -5,7 +5,6 @@ package com.bloom.domain.gardener;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.anyString;
