a75f7083604a5b07e0bd77cfb6926c7cc2370e46
xuliangliang <835547206@qq.com>
2019-01-06 14:50:37 +0800

    修改Gardener.java

--- src/main/java/com/bloom/dao/po/Gardener.java
+++ src/main/java/com/bloom/dao/po/Gardener.java
@@ -1,10 +1,15 @@
 package com.bloom.dao.po;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public class Gardener {
+import lombok.Data;
+@Data
+public class Gardener implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	private Integer id;
 
@@ -26,83 +31,4 @@ public class Gardener {
 
     private Date ut;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Integer roleId) {
-        this.roleId = roleId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username == null ? null : username.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName == null ? null : nickName.trim();
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender == null ? null : gender.trim();
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email == null ? null : email.trim();
-    }
-
-    public Date getCt() {
-        return ct;
-    }
-
-    public void setCt(Date ct) {
-        this.ct = ct;
-    }
-
-    public Date getUt() {
-        return ut;
-    }
-
-    public void setUt(Date ut) {
-        this.ut = ut;
-    }
 }
